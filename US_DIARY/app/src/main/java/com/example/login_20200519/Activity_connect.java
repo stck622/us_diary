9d75d1a1c807a6d3e8c4703d4825c15d7a0301c1
Leejimin <outum1500@gmail.com>
2020-06-01 14:51:23 +0900

    xml fix

--- US_DIARY/app/src/main/java/com/example/login_20200519/Activity_connect.java
+++ US_DIARY/app/src/main/java/com/example/login_20200519/Activity_connect.java
@@ -1,14 +1,48 @@
 package com.example.login_20200519;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 
 public class Activity_connect extends AppCompatActivity {
+
+    TextView tv_waiting;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener firebaseAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connect);
+        firebaseAuth = FirebaseAuth.getInstance();
+        tv_waiting = (TextView)findViewById(R.id.tv_waiting);
+
+        firebaseAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    tv_waiting.setText(user.getDisplayName().toString());
+                    Log.e(this.getClass().getName(), user.getDisplayName().toString());
+                } else {
+                }
+            }
+        };
     }
 }
